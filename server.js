5a88f0343578cf9b69f2db7e9756cf5a9d2a01a0
Omar Mohamed <125928590+omarmohamed286@users.noreply.github.com>
2024-12-16 19:46:06 +0200

    mongoose initialized

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 dotenv.config({path:"config.env"})
+mongoose.connect(process.env.DB_URI)
 
 const app = express()
 
 const PORT = process.env.PORT || 8000
+
+if(process.env.NODE_ENV == 'Development'){
+    app.use(morgan('dev'))
+    console.log(`Mode: ${process.env.NODE_ENV}`)
+}
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
